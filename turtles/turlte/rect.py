f32d390f192f4811b37a4fa7ff5f495d2b4c10cf
Thuan <ttctde@gmail.com>
2022-01-28 18:17:48 +0700

    Gui

---
+++ turtles/turlte/rect.py
@@ -0,0 +1,20 @@
+from turtle import *
+
+
+forward(100)
+
+right(90)
+
+
+pencolor('red')
+forward(100)
+right(90)
+
+pencolor('green')
+forward(100)
+right(90)
+
+pencolor('blue')
+forward(100)
+right(90)
+done()
